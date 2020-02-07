9570aebae3b254d751656f7e532de50cfff64da3
“patrick-huber-pivotal” <patrick-huber-pivotal@users.noreply.github.com>
2020-02-07 13:21:22 +0000

    updates bucket vars, adds new pipeline

--- ci/set-pipelines.sh
+++ ci/set-pipelines.sh
@@ -3,8 +3,14 @@
 # get the current script directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+echo "setting dependencies pipeline"
 fly -t dell set-pipeline \
    -p retrieve-platform-dependencies \
    -c $DIR/dependencies.yml \
    -l $DIR/vars.yml
 
+echo "setting installation pipeline"
+fly -t dell set-pipeline \
+   -p installation \
+   -c $DIR/installation.yml \
+   -l $DIR/vars.yml
